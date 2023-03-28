6c8858e82be3f7f73d799d263540dd84b518a6a6
Mustufa-kerawala <mustufakerawala@outlook.com>
2023-03-28 09:32:06 +0300

    Fetching data and rendering on the website

---
+++ index.js
@@ -0,0 +1,17 @@
+const mainCotainer = document.querySelector('.container')
+
+
+// Getting data from the server 
+fetch('https://jsonplaceholder.typicode.com/posts/')
+.then(res => res.json())
+.then(elements => elements.forEach((element) => {
+//    Creating a div for each element
+    const elementDiv = document.createElement('div')
+    // Creating h2 and p for title and body
+    const title = document.createElement('h2')
+    const body = document.createElement('p')
+    title.innerText = element.title
+    body.innerText = element.body
+    elementDiv.appendChild(title, body)
+    mainCotainer.append(elementDiv)
+}))
